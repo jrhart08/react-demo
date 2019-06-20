21131d53020ee59ec03f2b34d17e276b7be96550
Joseph Hart <josephrhart@yahoo.com>
2019-06-19 19:05:40 -0700

    adding useState example

--- slide-examples/2. higher-order-components/keylogger.jsx
+++ slide-examples/2. higher-order-components/keylogger.jsx
@@ -1,11 +1,22 @@
 /* eslint-disable */
 
-const withClickLogging = (Component) => ({ onClick, ...rest }) => {
-  const logOnClick = (e) => {
-    console.log(`clicked on ${Component.name}`);
+const withClickLogging = (Component) => {
+  const LoggerComponent = ({ onClick, ...rest }) => {
+    const logOnClick = (e) => {
+      console.log(`clicked on ${Component.name}`);
 
-    return onClick && onClick(e);
+      return onClick && onClick(e);
+    };
+
+    return <Component onClick={logOnClick} {...rest} />;
   };
 
-  return <Component onClick={logOnClick} {...rest} />;
-}
+  LoggerComponent.name = `withLogging(${Component.name})`;
+
+  return LoggerComponent;
+};
+
+// elsewhere
+const ComponentWithLogging = withClickLogging(props => <div>{props}</div>);
+
+export default withClickLogging;
